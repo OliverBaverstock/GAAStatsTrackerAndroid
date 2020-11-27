9e18ce5c3071ff189a6de85eefb3181fed01978b
OliverBaverstock <oliverbaverstock@yahoo.ie>
2020-11-27 07:41:28 +0000

    Welcome Screen Designed

---
+++ app/src/main/java/oliver/gaa_stats_tracker/MainActivity.kt
@@ -0,0 +1,11 @@
+package oliver.gaa_stats_tracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.welcome_page)
+    }
+}
